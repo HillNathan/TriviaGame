541824395ba9c5eb3adad4dd0932b34e96499b87
Nathan Hill <pjsstp@gmail.com>
2019-06-12 09:02:20 -0400

    added functions for timing, checking guesses, ending the game, and updating the display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,107 @@
 $(document).ready(function() {
 
-    var myArray = [];
-    for (i=0; i<TriviaQuestions.length; i++) {
-        myArray.push(i);
+    var gameObj = {
+        questionDisplay: $("#display-div"),
+        questionNum: 0,
     }
-    console.log(myArray);
 
-    shuffle(myArray);
+    var questionTimeout;
+    var answerTimeout;
+    var numQuestions = 0;
+    var correct = 0;
+    var wrong = 0;
+    var noAnswer = 0;
+    var response1 = $("#button-1");
+    var response2 = $("#button-2");
+    var response3 = $("#button-3");
+    var response4 = $("#button-4");
+
+
+    var questionOrder = [];
+    for (i=0; i<TriviaQuestions.length; i++) {
+        questionOrder.push(i);
+    }
+    shuffle(questionOrder);
+    numQuestions = questionOrder.length;
 
     $(".start-button").click(function() {
-        console.log(myArray.pop());
-        console.log(myArray);
+        getQuestion();
     });
-    $("#button-1").click(function() {
-        alert("Button 1");
+
+    response1.click(function() {
+        checkGuess(this.value); 
     })
-    $("#button-2").click(function() {
-        alert("Button 2");
+    response2.click(function() {
+        checkGuess(this.value);
     })
-    $("#button-3").click(function() {
-        alert("Button 3");
+    response3.click(function() {
+        checkGuess(this.value);
     })
-    $("#button-4").click(function() {
-        alert("Button 4");
+    response4.click(function() {
+        checkGuess(this.value);
     })
+
+    function getQuestion() {    
+        if (!questionTimeout) {
+            answerTimeout = undefined;
+            $(".response-btn").show();
+            if (questionOrder.length !== 0) {
+                gameObj.questionNum = questionOrder.pop()
+                gameObj.questionDisplay.html(TriviaQuestions[gameObj.questionNum].theQuestion);
+                response1.text(TriviaQuestions[gameObj.questionNum].opt1);
+                response2.text(TriviaQuestions[gameObj.questionNum].opt2);
+                response3.text(TriviaQuestions[gameObj.questionNum].opt3);
+                response4.text(TriviaQuestions[gameObj.questionNum].opt4);
+                questionTimeout = setTimeout( function() {
+                    noGuess();
+                }, 5000 );
+            }
+            else {
+                gameObj.questionDisplay.html("<span class='answer-text'>Game Over</span>" + 
+                // "<p>Questions Asked: " + numQuestions + "</p>" + 
+                "<p>Correct answers: " + correct + "</p>" + 
+                "<p>Wrong answers: " + wrong + "</p>" + 
+                "<p>Questions missed: " + noAnswer + "</p>" + 
+                "<br><button class='start-button btn btn-lg sedgwick' id='play-again'>Play again!</button>"
+                );
+                $(".response-btn").hide();
+            }
+        }
+    }
+
+    function checkGuess(myGuess) {
+        if (!answerTimeout) {
+            clearTimeout(questionTimeout);
+            questionTimeout = undefined;
+            if (myGuess === TriviaQuestions[gameObj.questionNum].correct) {
+                console.log("You have chosen wisely.");
+                // questionDisplay.empty();
+                gameObj.questionDisplay.html("<span class='answer-text'>You have chosen wisely.</span>");
+                correct++;
+            }
+            else {
+                gameObj.questionDisplay.html("<span class='answer-text'>You have chosen poorly.</span>");
+                console.log("You have chosen poorly.");
+                wrong++
+            }
+            answerTimeout = setTimeout(getQuestion, 3000);
+        }
+    }
+
+    function noGuess() {
+        if (!answerTimeout) {
+            questionTimeout = undefined;
+            questionDisplay.html("<span class='answer-text'>You're out of time for this one.</span>");
+            console.log("Time is up!");
+            noAnswer++;
+        }
+        answerTimeout = setTimeout(getQuestion, 3000);
+    }
+
+
+
+
+
 
 
 
@@ -45,3 +123,4 @@ function shuffle (array) {
     }
     return array;
 }
+
--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,6 +1,5 @@
 var TriviaQuestions = [
     { theQuestion: "When did Pearl Jam release their album, 'Ten'?", 
-        chosen: false,
         opt1: '1989',
         opt2: '1991',
         opt3: '1993',
@@ -8,7 +7,6 @@ var TriviaQuestions = [
         correct: 'opt2' },
     
     { theQuestion: "What band did the drummer from Nirvana go on to form after the death of Kurt Cobain in 1994?",
-        chosen: false,
         opt1: 'Foo Fighters',
         opt2: 'Mother Love Bone',
         opt3: 'Alice in Chains',
@@ -16,7 +14,6 @@ var TriviaQuestions = [
         correct: 'opt1' },
 
     { theQuestion: "What project formed as a response to the death of Andrew Wood, the lead singer of Mother Love Bone?",
-        chosen: false,
         opt1: 'Mad Season',
         opt2: 'Pearl Jam',
         opt3: 'Green River',
@@ -24,7 +21,6 @@ var TriviaQuestions = [
         correct: 'opt4' },
 
     { theQuestion: "What group did NOT include vocals by singer Chris Cornell?",
-        chosen: false, 
         opt1: 'Soundgarden',
         opt2: 'Audioslave',
         opt3: 'Temple of the Dog',
