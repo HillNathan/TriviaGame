e4c806a939fbd514718c7dab078934226e4ffdf8
Nathan Hill <pjsstp@gmail.com>
2019-06-12 06:37:06 -0400

    built method to determine quetion order with array and shuffle function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,47 @@
 $(document).ready(function() {
 
+    var myArray = [];
+    for (i=0; i<TriviaQuestions.length; i++) {
+        myArray.push(i);
+    }
+    console.log(myArray);
 
-    console.log(TriviaQuestions[0].correct);
-
-    console.log(TriviaQuestions[1].correct);
-
+    shuffle(myArray);
 
     $(".start-button").click(function() {
-        alert("Start the game already!!!");
+        console.log(myArray.pop());
+        console.log(myArray);
     });
+    $("#button-1").click(function() {
+        alert("Button 1");
+    })
+    $("#button-2").click(function() {
+        alert("Button 2");
+    })
+    $("#button-3").click(function() {
+        alert("Button 3");
+    })
+    $("#button-4").click(function() {
+        alert("Button 4");
+    })
+
+
+
+
 
 });
+
+function shuffle (array) {
+    // returns a shuffled array, does not keep the original, unshuffled array
+    var currentIndex = array.length;
+    var tempValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        tempValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = tempValue;
+    }
+    return array;
+}
